dcb3067c7f27ce0af9460763e87c0203c6c8b113
Binnaz Akyüz <binnaz.akyuz@cmeducations.se>
2022-08-24 13:17:07 +0200

    Add config db.js

--- data/db.js
+++ data/db.js
@@ -1,6 +1,6 @@
 const config = require("./knexfile");
 const knex = require("knex");
 
-const db = knex(config["development"]);
+const db = knex(config[process.env.NODE_ENV]);
 
 module.exports = db;
